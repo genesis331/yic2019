22e14b01e8f8166b460130f7307f0a780a6b0f37
genesis331 <cheah3838@gmail.com>
2019-09-12 19:35:38 +0800

    Init App Code for Client App
    Signed-off-by: genesis331 <cheah3838@gmail.com>

---
+++ yic2019client/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme:
+      ThemeData(primarySwatch: Colors.indigo, fontFamily: 'Product Sans'),
+      home: LoadingScreen(),
+    );
+  }
+}
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+//  var upcomingVisitorsRef =
+//  FirebaseDatabase.instance.reference().child('data').limitToFirst(5);
+
+  @override
+  void initState() {
+    super.initState();
+//    databaseReference.child('/data').once().then((DataSnapshot snapshot) {
+//      return snapshot.value != null
+//          ? Navigator.of(context).pushReplacement(PageRouteBuilder(
+//        pageBuilder: (c, a1, a2) => HomePage(),
+//        transitionsBuilder: (c, anim, a2, child) =>
+//            FadeTransition(opacity: anim, child: child),
+//        transitionDuration: Duration(milliseconds: 1500),
+//      ))
+//          : Scaffold.of(context).showSnackBar(SnackBar(
+//          content:
+//          Text('Please make sure you\'re connected to the Internet.')));
+//    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Text('IntelliGuard',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 38,
+                            fontWeight: FontWeight.bold)),
+                    Text('for Clients',
+                        style: TextStyle(color: Colors.white, fontSize: 22)),
+//                    Container(
+//                      padding: EdgeInsets.only(top: 50),
+//                      child: CircularProgressIndicator(),
+//                    )
+                  ],
+                ),
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          )),
+    );
+  }
+}
