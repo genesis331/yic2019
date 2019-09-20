c19596247cb0b846acf007dee1ae18536cf57530
genesis331 <cheah3838@gmail.com>
2019-09-20 12:39:40 +0800

    Added barcode generator
    Signed-off-by: genesis331 <cheah3838@gmail.com>

--- yic2019client/barcode.php
+++ yic2019client/barcode.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 $visitid = $_SESSION['visitid'];
-//echo $visitid;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,14 +9,23 @@ $visitid = $_SESSION['visitid'];
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Barcode</title>
+	<script src="https://cdn.jsdelivr.net/jsbarcode/3.6.0/JsBarcode.all.min.js"></script>
 </head>
 <link rel="stylesheet" href="barcode.css">
 <body>
 
    <h1>Here is your barcode, thanks for your cooperation	</h1>
-   <div class="center-container"><div style="width:300px;height:100px;border:1px solid #000;">barcode</div></div>
+   <div class="center-container"><div style="width:300px;height:100px;border:1px solid #000;">
+   <img id="barcode"/>
+   </div></div>
 	<p>Please bring along your Identity Card for your vertification</p>
 	<p>Please be punctual! </p>
-
+<script>
+	JsBarcode("#barcode", $visitid, {
+      format: "pharmacode",
+      lineColor: "#000000",
+      displayValue: false
+    });
+	</script>
 </body>
 </html>
